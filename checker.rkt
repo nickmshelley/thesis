999b7b1ae2a4db7c9a1c25bbfa307202f321a725
Nick Shelley <nickmshelley@gmail.com>
2012-12-08 11:24:42 -0700

    Parallelized checker.

--- checker.rkt
+++ checker.rkt
@@ -7,6 +7,7 @@
          racket/system
          racket/string
          racket/port
+         racket/place
          plot
          (only-in srfi/13 string-suffix?))
 
@@ -34,12 +35,126 @@
                        (word-results->string wr2))
                #f))))
 
-(define (check-all-files files file-mod)
-  (define percent 1)
+(define (check-all-files/places/remove files percent)
+  (define num-workers (* 2 (processor-count)))
+  (define places (for/list ([i (in-range num-workers)]
+                            [file files])
+                   (define p (make-worker-place/remove))
+                   (place-channel-put p i)
+                   (place-channel-put p percent)
+                   p))
+  (define result-messages (do-all-work-on-places files places))
+  (map list->results result-messages))
+
+(define (check-all-files/places/truncate files percent)
+  (define num-workers (* 2 (processor-count)))
+  (define places (for/list ([i (in-range num-workers)]
+                            [file files])
+                   (define p (make-worker-place/truncate))
+                   (place-channel-put p i)
+                   (place-channel-put p percent)
+                   p))
+  (define result-messages (do-all-work-on-places files places))
+  (map list->results result-messages))
+
+;; do-all-work-on-places : list-of-place-message list-of-place -> list-of-result-messages
+;; number of messages has to be >= number of places
+(define (do-all-work-on-places messages places)
+  ; hand out initial messages to get all places working
+  (for ([m messages]
+        [p places])
+    (place-channel-put p m))
+  ; hand out remaining messages
+  (define remaining-messages (list-tail messages (length places)))
+  (define partial-answers 
+    (pass-out-remaining-messages remaining-messages places))
+  (define remaining-answers
+    (wait-until-all-done places))
+  (append partial-answers remaining-answers))
+
+(define (pass-out-remaining-messages messages places)
+  (let loop ([remaining messages] [answers empty])
+      (if (empty? remaining)
+          answers
+          (apply sync
+                 (for/list ([e (in-list places)])
+                   (handle-evt e (lambda (ans)
+                                   (place-channel-put e (first remaining))
+                                   (loop (rest remaining) 
+                                         (cons ans answers)))))))))
+
+
+(define (wait-until-all-done evts)
+  (let loop ([left evts] [answers empty])
+    (if (empty? left)
+        answers
+        (apply sync
+               (for/list ([e (in-list left)])
+                 (handle-evt e (lambda (ans) 
+                                 (loop (remove e left) 
+                                       (cons ans answers)))))))))
+
+(define (make-worker-place/remove)
+  (place ch
+    (define place-number (place-channel-get ch))
+    (define percent (place-channel-get ch))
+    (let loop ()
+      (define file (place-channel-get ch))
+      (define res (check-file/remove file percent place-number))
+      (place-channel-put ch (results->list res))
+      (loop))))
+
+(define (make-worker-place/truncate)
+  (place ch
+    (define place-number (place-channel-get ch))
+    (define percent (place-channel-get ch))
+    (let loop ()
+      (define file (place-channel-get ch))
+      (define res (check-file/truncate file percent place-number))
+      (place-channel-put ch (results->list res))
+      (loop))))
+
+; check-file : string -> results
+(define (check-file/remove filename percent place-number)
+  (define file-string (file->string filename))
+  (define words (percent-of-words-from-file percent file-string))
+  (results 
+   (path->string filename)
+   (for/list ([word words])
+     (check-word word string-w/o-word file-string place-number))))
+
+; check-file : string -> results
+(define (check-file/truncate filename percent place-number)
+  (define file-string (file->string filename))
+  (define words (percent-of-words-from-file percent file-string))
+  (results 
+   (path->string filename)
+   (for/list ([word words])
+     (check-word word string-truncated-from-word file-string place-number))))
+
+(define (list->results l)
+  (results (first l) (map list->word-results (second l))))
+
+(define (list->word-results l)
+  (apply word-results l))
+
+(define (results->list res)
+  (list (results-filename res)
+        (map word-results->list (results-wordsults res))))
+
+(define (word-results->list wordsults)
+  (list (word->list (word-results-word wordsults))
+        (word-results-passed wordsults)
+        (word-results-failed-messages wordsults)))
+
+(define (word->list w)
+  (list (word-str w) (word-pos w)))
+
+(define (check-all-files files file-mod percent)
   (map (λ (filename)
          (check-file file-mod filename percent))
        files))
-
+    
 ; check-file : string -> results
 (define (check-file file-mod filename percent)
   (define file-string (file->string filename))
@@ -47,21 +162,21 @@
   (results 
    (path->string filename)
    (for/list ([word words])
-     (check-word word file-mod file-string))))
+     (check-word word file-mod file-string 1))))
 
 ; check-word : word string -> word-results
 ; runs the programs resulting from replacing a word by all of its completions
-(define (check-word word file-mod file-string)
+(define (check-word word file-mod file-string place-number)
   (define altered-string (file-mod file-string word))
   (define completions (get-completions altered-string ""))
-  (define temp-file "/tmp/file.rkt")
+  (define temp-file (format "/tmp/file-~a.rkt" place-number))
   (define res
     (for/list ([completion completions])
       (with-output-to-file temp-file
         (lambda () 
           (display (replace-word-with-string word completion file-string)))
         #:exists 'replace)
-      (define error-string (open-output-string)) ;will use this later
+      (define error-string (open-output-string))
       (define success?
         (parameterize ([current-error-port error-string]
                        [current-output-port (open-output-nowhere)])
@@ -74,12 +189,12 @@
   (word-results word (length passed) messages))
 (module+ test
   (define str "#lang racket (define x 2) (+ x x) ;y")
-  (define results (check-word (word "x" 29) string-w/o-word str))
+  (define results (check-word (word "x" 29) string-w/o-word str 1))
   (check-true (word-results-equal?
                results
                (word-results (word "x" 29) 2 (make-list 5 "error"))))
   (set! str "#lang racket (define x 2) (+ x x) ;y")
-  (set! results (check-word (word "x" 29) string-truncated-from-word str))
+  (set! results (check-word (word "x" 29) string-truncated-from-word str 1))
   (check-true (word-results-equal?
                results
                (word-results (word "x" 29) 2 (make-list 4 "error")))))
@@ -167,18 +282,18 @@
   (results "All" (append (results-wordsults r1) (results-wordsults r2))))
 
 (module+ main
+  (define percent 1)
   (when (not (directory-exists? "output"))
     (make-directory "output"))
   (when (not (directory-exists? "output/checker"))
     (make-directory "output/checker"))
-  (define remove 
-    (check-all-files 
-     (get-all-source-files "test-files/checker-source")
-     string-w/o-word))
-  (define truncate 
-    (check-all-files 
-     (get-all-source-files "test-files/checker-source")
-     string-truncated-from-word))
+  (define source-files (get-all-source-files "test-files/checker-source"))
+  (define remove
+    (check-all-files/places/remove source-files percent)
+    #;(check-all-files source-files string-w/o-word percent))
+  (define truncate
+    (check-all-files/places/truncate source-files percent)
+    #;(check-all-files source-files string-truncated-from-word percent))
   (for-each display-results remove truncate)
   (define remove-sum (foldl add-results
                             (results "All" empty)
