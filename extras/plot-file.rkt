15fc258292ba3b3ab2d01c09aa5781d011a1757c
Nick Shelley <nickmshelley@gmail.com>
2014-02-20 21:31:01 -0700

    Added checker graphs.

--- extras/plot-file.rkt
+++ extras/plot-file.rkt
@@ -4,6 +4,28 @@
 (struct rankings (filename ranked unranked missed) #:transparent)
 
 (module+ main
+  #;(produce-ranker-graphs)
+  (produce-checker-graphs))
+
+(define (produce-checker-graphs)
+  (define remove-results
+    (list (list 'proximity 2294 11789)
+          (list 'nest 2399 11263)
+          (list 'keywords 2108 14751)
+          (list 'macros 1701 16867)
+          (list 'bytecode 1425 15301)
+          (list 'combined 2496 11131)))
+  (display-checker remove-results "Remove")
+  (define truncate-results
+    (list (list 'proximity 2228 11758)
+          (list 'nest 2309 11573)
+          (list 'keywords 2324 13168)
+          (list 'macros 1667 16840)
+          (list 'bytecode 1487 15109)
+          (list 'combined 2246 11904)))
+  (display-checker truncate-results "Truncate"))
+
+(define (produce-ranker-graphs)
   (define remove-results
     (list (cons 'proximity (rankings "All" '(88 131 244 392 284) 3448 404))
           (cons 'nest (rankings "All" '(144 593 463 482 325) 2566 418))
@@ -24,6 +46,26 @@
   (display-rankings truncate-results "Truncate")
   (display-combined truncate-results "Truncate-combined")
   (display-uber-combined truncate-results "Truncate-uber-combined"))
+
+(define (display-checker results name)
+  (define skip (+ 3 (length results)))
+  (plot-file
+   (for/list ([res (in-list results)]
+              [i (in-range (length results))])
+     (define label (symbol->string (first res)))
+     (define passed (second res))
+     (define failed (third res))
+     (define color (add1 i))
+     (discrete-histogram
+      (append (list (vector 'Passed passed))
+              (list (vector 'Failed failed)))
+      #:skip skip #:x-min i #:y-min .1 #:label label #:color color #:line-color color))
+   (format "/Users/heather/Nick/thesis/thesis/output/synthesis/checker/~a.png" name)
+   #:title name
+   #:x-label "Rank"
+   #:y-label "Amount"
+   #:height 800
+   #:width 1800))
 
 (define (display-rankings results name)
   (define skip (+ 3 (length results)))
@@ -77,6 +119,26 @@
       (list (vector 'Hit (+ (apply + (rankings-ranked current-rankings))
                             (rankings-unranked current-rankings)))
             (vector 'Missed (rankings-missed current-rankings)))
+      #:skip skip #:x-min i #:y-min .1 #:label label #:color color #:line-color color))
+   (format "/Users/heather/Nick/thesis/thesis/output/synthesis/ranker/~a.png" name)
+   #:title name
+   #:x-label "Rank"
+   #:y-label "Amount"
+   #:height 800
+   #:width 1800))
+
+(define (display-checked results name)
+  (define skip (+ 3 (length results)))
+  (plot-file
+   (for/list ([res (in-list results)]
+              [i (in-range (length results))])
+     (define label (symbol->string (car res)))
+     (define current-rankings (cdr res))
+     (define color (add1 i))
+     (discrete-histogram
+      (append (ranked->vectors (rankings-ranked current-rankings))
+              (list (vector 'Unranked (rankings-unranked current-rankings)))
+              (list (vector 'Missed (rankings-missed current-rankings))))
       #:skip skip #:x-min i #:y-min .1 #:label label #:color color #:line-color color))
    (format "/Users/heather/Nick/thesis/thesis/output/synthesis/ranker/~a.png" name)
    #:title name
